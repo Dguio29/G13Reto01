3eaf3f7e8bb03daa23d771986d3101e602b01256
Dguio29 <d.guio29@hotmail.com>
2024-09-02 17:55:56 -0500

    m

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
         // Datos de ejemplo
         const experiencias = [
-            { puesto: "Desarrollador Web", empresa: "Empresa A", periodo: "2020 - Presente", descripcion: "Desarrollo de aplicaciones web utilizando HTML, CSS y JavaScript." },
-            { puesto: "Diseñador UX", empresa: "Empresa B", periodo: "2018 - 2020", descripcion: "Diseño de interfaces de usuario y experiencia de usuario para aplicaciones móviles." }
+            { puesto: "Analista BI y automatización de procesos Experiencia Cliente", empresa: "Indra Mint Sait", periodo: "2020 - Presente", descripcion: "Operar plataformas de actualización Data Integration, Arquitectura y creación de procesos de ejecución en SQL, Power BI, importaciones, procedimientos de almacenado, para la creación de reportes gerenciales con conexiones directamente de DDBB (SQL)" },
+            { puesto: "Analista en Automatización", empresa: "Americas Bussines", periodo: "2018 - 2020", descripcion: "Operar plataformas de actualización Data Integration, crear procesos de ejecución en SQL, power BI y ACCESS, importaciones, procedimientos de almacenado, para la creación de reportes gerenciales con conexiones directamente de DDBB (SQL), donde se conecta con PI creando tablas, vistas, procedimientos de almacenado, depuración y tratamiento de bases de datos, entre otros realizar conexiones con office para la elaboración de informes y procesos, para ser visualizado POWER BI." }
         ];
 
         const educacion = [
-            { titulo: "Grado en Ingeniería Informática", institucion: "Universidad XYZ", año: "2018" },
-            { titulo: "Máster en Desarrollo Web", institucion: "Instituto ABC", año: "2020" }
+            { titulo: "Ingeniería sistemas 3er semestre", institucion: "CUN", año: "2023" },
+            { titulo: "Técnico en Programación de Software", institucion: "SENA", año: "2017" }
         ];
 
         // Función para añadir experiencias
